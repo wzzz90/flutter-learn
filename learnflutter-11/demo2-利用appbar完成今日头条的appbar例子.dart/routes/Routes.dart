c28e7638c5eead9765626d72d9b931e758458ee6
timer09 <1522935827@qq.com>
2019-07-18 15:14:28 +0800

    xxx

---
+++ learnflutter-11/demo2-利用appbar完成今日头条的appbar例子.dart/routes/Routes.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import '../pages/Tabs.dart';
+import '../pages/AppBarDemo.dart';
+
+final routes = {
+  '/': (context) => Tabs(),
+  '/appbar': (context) => AppbarPage()
+};
+
+
+var onGenerateRoute = (RouteSettings settings) {
+  final String name = settings.name;
+  final Function pageContentBuilder = routes[name];
+
+  if(pageContentBuilder != null) {
+    if(settings.arguments != null) {
+      final Route route = MaterialPageRoute(
+        builder: (context) => pageContentBuilder(context, arguments: settings.arguments)
+      );
+      return route;
+    } else {
+      final Route route = MaterialPageRoute(
+        builder: (context) => pageContentBuilder(context)
+      );
+      return route;
+    }
+  }
+};
